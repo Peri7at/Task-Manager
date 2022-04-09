74b058c15884eeb4119ff9b78d74d7782a650441
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-04-09 19:14:39 +0300

    v1

---
+++ starter/models/Task.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Must provide a task name'],
+    trim: true,
+    maxlength: [30, 'A task name can not be more than 30 characters']
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
